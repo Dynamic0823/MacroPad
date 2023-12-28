e082c35c75089f31bac4e42eb3d9ac4ebe027a42
aaron <hanleyaaron23@gmail.com>
2023-12-28 09:44:35 -0500

    Now we read the gpio port register for columns instead of doing 4 digitalReads. 	-Soldered Pull Down Resistors to Pull down lines 	-enc butt needs pull down 	-change target row/col each successful press, 	-looking into keyboard input next

--- Code/macropad_arduino/macropad_arduino.ino
+++ Code/macropad_arduino/macropad_arduino.ino
@@ -1,63 +1,54 @@
 #define RXLED 17
-#define COL0 1
-#define COL1 0
-#define COL2 2
-#define COL3 3
-#define ROW2 5
-#define ROW1 6
-#define ROW0 7
 #define ENCBUT 4
-int rowstate=0;
-uint8_t readport;
-int readstates[3][4];
-uint8_t read = 0;
+static int8_t columns[]={1,0,2,3};
+static int8_t rows[]={7,6,5};
+static uint8_t col_bits[4];
+volatile uint8_t reads[]={0,0,0};
+uint8_t readport=0;
+volatile int8_t rowstate=0;
+uint8_t target_col=0;
+uint8_t target_row = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(RXLED, OUTPUT);
-  pinMode(COL0, INPUT);
-  pinMode(COL1, INPUT);
-  pinMode(COL2, INPUT);
-  pinMode(COL3, INPUT);
-  pinMode(ROW0, OUTPUT);
-  pinMode(ROW1, OUTPUT);
-  pinMode(ROW2, OUTPUT);
   digitalWrite(RXLED,HIGH);
-  digitalWrite(ROW0,LOW);
-  digitalWrite(ROW1,LOW);
-  digitalWrite(ROW2,LOW);
   for(uint8_t c = 0; c<3; c++){
-    for(uint8_t d = 0; d<4; d++){
-      readstates[c][d] = LOW;
-    }
+    pinMode(rows[c], OUTPUT);
+    digitalWrite(rows[c],LOW);
   }
+  for(uint8_t d = 0; d<4; d++){
+    pinMode(columns[d], INPUT);
+    col_bits[d]= digitalPinToBitMask(columns[d]);
+  }
+  readport = digitalPinToPort(columns[0]);
 }
 
 void loop() {
-  if(rowstate == 0){
-    digitalWrite(ROW0,HIGH);
-  }else if(rowstate==1){
-    digitalWrite(ROW1,HIGH);
-  }else if(rowstate==2){
-    digitalWrite(ROW2,HIGH);
-  }
-  readstates[rowstate][0] = digitalRead(COL0);
-  readstates[rowstate][1] = digitalRead(COL1); 
-  readstates[rowstate][2] = digitalRead(COL2);
-  readstates[rowstate][3] = digitalRead(COL3); 
-    if(readstates[0][0] == HIGH){
+  digitalWrite(rows[rowstate],HIGH);
+  reads[rowstate] = *portInputRegister(readport);
+    if((reads[target_row] & col_bits[target_col]) && (rowstate == target_row)){
       digitalWrite(RXLED, LOW);
+      target_col++;
+      if(target_col==4){
+        target_col=0;
+        target_row++;
+        if(target_row==3){
+          target_row = 0;
+        }
+      }
+        delay(100);
     }else{  
       digitalWrite(RXLED, HIGH);
     }
-  
-  digitalWrite(ROW0,LOW);
-  digitalWrite(ROW1,LOW);
-  digitalWrite(ROW2,LOW);
+
+  digitalWrite(rows[0],LOW);
+  digitalWrite(rows[1],LOW);
+  digitalWrite(rows[2],LOW);
 
   rowstate+=1;
   if(rowstate >2){
     rowstate = 0;
   }
-  delay(10);
+  delay(1);
 
 }
